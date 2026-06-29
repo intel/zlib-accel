466c76d55c244e70eee5114856b870622fa438e5
Mohamed Issa <mohamed.issa@intel.com>
2026-06-29 00:14:27 -0700

    Rewrite sharded map data structure

--- config/config.cpp
+++ config/config.cpp
@@ -15,21 +15,22 @@ std::string log_file = "";
 
 // default config values initialization
 uint32_t configs[CONFIG_MAX] = {
-    1,   /*use_qat_compress*/
-    1,   /*use_qat_uncompress*/
-    0,   /*use_iaa_compress*/
-    0,   /*use_iaa_uncompress*/
-    1,   /*use_zlib_compress*/
-    1,   /*use_zlib_uncompress*/
-    50,  /*iaa_compress_percentage*/
-    50,  /*iaa_uncompress_percentage*/
-    0,   /*iaa_prepend_empty_block*/
-    0,   /*qat_periodical_polling*/
-    1,   /*qat_compression_level*/
-    0,   /*qat_compression_allow_chunking*/
-    0,   /*ignore_zlib_dictionary*/
-    1,   /*log_level*/
-    1000 /*log_stats_samples*/
+    1,    /*use_qat_compress*/
+    1,    /*use_qat_uncompress*/
+    0,    /*use_iaa_compress*/
+    0,    /*use_iaa_uncompress*/
+    1,    /*use_zlib_compress*/
+    1,    /*use_zlib_uncompress*/
+    50,   /*iaa_compress_percentage*/
+    50,   /*iaa_uncompress_percentage*/
+    0,    /*iaa_prepend_empty_block*/
+    0,    /*qat_periodical_polling*/
+    1,    /*qat_compression_level*/
+    0,    /*qat_compression_allow_chunking*/
+    0,    /*ignore_zlib_dictionary*/
+    1,    /*log_level*/
+    1000, /*log_stats_samples*/
+    64    /*map_shards*/
 };
 
 bool LoadConfigFile(std::string& file_content, const char* file_path) {
@@ -53,7 +54,8 @@ bool LoadConfigFile(std::string& file_content, const char* file_path) {
 	  "qat_compression_allow_chunking",
     "ignore_zlib_dictionary",
     "log_level",
-    "log_stats_samples"
+    "log_stats_samples",
+    "map_shards"
   };
   // clang-format on
 
@@ -87,7 +89,7 @@ bool LoadConfigFile(std::string& file_content, const char* file_path) {
   trySetConfig(IGNORE_ZLIB_DICTIONARY, 1, 0);
   trySetConfig(LOG_LEVEL, 3, 0);
   trySetConfig(LOG_STATS_SAMPLES, UINT32_MAX, 0);
-
+  trySetConfig(MAP_SHARDS, 65536, 1);
   config_reader.GetValue("log_file", log_file);
   file_content.append(config_reader.DumpValues());
 
--- sharded_map.h
+++ sharded_map.h
@@ -3,45 +3,89 @@
 
 #pragma once
 
+#include <cassert>
 #include <functional>
-#include <shared_mutex>
+#include <memory>
+#include <oneapi/tbb/concurrent_hash_map.h>
 #include <string>
-#include <unordered_map>
 #include <utility>
 
-static constexpr int SHARDS = 64;
+#include "config/config.h"
+
+using namespace config;
 
 template <typename Key, typename Value>
 class ShardedMap {
  public:
-  auto Get(Key key) -> decltype(std::declval<Value>().get()) {
-    unsigned int shard = GetShard(key);
-    std::shared_lock<std::shared_mutex> lock(shard_mutexes[shard]);
-    auto it = map[shard].find(key);
-    if (it == map[shard].end()) {
+  ShardedMap(void) {
+    const auto num_shards = configs[MAP_SHARDS];
+    pd_map_arr = std::make_unique<PaddedMapType[]>(num_shards);
+  }
+
+  ~ShardedMap(void) {
+    pd_map_arr.reset();
+  }
+
+  auto Get(const Key& key) -> decltype(std::declval<Value>().get()) {
+    const auto shard = GetShard(key);
+    typename MapType::const_accessor acc;
+    if (!pd_map_arr[shard].map.find(acc, key)) {
       return nullptr;
     }
-    return it->second.get();
+    return acc->second.get();
   }
 
-  void Set(Key key, Value&& value) {
-    unsigned int shard = GetShard(key);
-    std::unique_lock<std::shared_mutex> lock(shard_mutexes[shard]);
-    map[shard][key] = std::move(value);
-  }
-
-  void Unset(Key key) {
-    unsigned int shard = GetShard(key);
-    std::unique_lock<std::shared_mutex> lock(shard_mutexes[shard]);
-    auto it = map[shard].find(key);
-    if (it != map[shard].end()) {
-      map[shard].erase(it);
+  void Set(const Key& key, Value&& value) {
+    const auto shard = GetShard(key);
+    typename MapType::accessor acc;
+    if (!pd_map_arr[shard].map.find(acc, key)) {
+      // Key doesn't exist - add entry first
+      pd_map_arr[shard].map.insert(acc, key);
     }
+
+    // Set or update value associated with key
+    acc->second = std::move(value);
+  }
+
+  void Unset(const Key& key) {
+    const auto shard = GetShard(key);
+    pd_map_arr[shard].map.erase(key);
   }
 
  private:
-  unsigned int GetShard(Key key) { return std::hash<Key>{}(key) % SHARDS; }
-  std::unordered_map<Key, Value> map[SHARDS];
-  std::shared_mutex shard_mutexes[SHARDS];
-  std::hash<std::string> hash;
+  // Number of bytes in a cache line
+  static constexpr std::size_t CACHE_LINE_SIZE = 64;
+
+  // Fibonacci hash constant: value is (2^64 / phi) to spread bits uniformly,
+  // where phi is golden ratio and 64 is machine word length - constant is
+  // multiplied by hash and then shifted right by top bits to get shard index
+  static constexpr std::size_t FIBONACCI_MULTIPLIER = 11400714819323198485ull;
+
+  struct HashCompare {
+    static auto hash(const Key& key) -> std::size_t {
+      return std::hash<Key>{}(key);
+    }
+
+    static auto equal(const Key& lhs, const Key& rhs) -> bool {
+      return lhs == rhs;
+    }
+  };
+
+  using MapType = oneapi::tbb::concurrent_hash_map<Key, Value, HashCompare>;
+  struct alignas(CACHE_LINE_SIZE) PaddedMapType {
+    MapType map;
+  };
+  std::unique_ptr<PaddedMapType[]> pd_map_arr;
+
+  // Number of map shards must be a power of 2
+  auto GetShard(const Key& key) const -> unsigned int {
+    const auto num_shards = configs[MAP_SHARDS];
+    assert((num_shards & (num_shards - 1)) == 0);
+    const auto shard_bits = __builtin_ctz(num_shards);
+    const auto hash = static_cast<uint64_t>(std::hash<Key>{}(key));
+    const auto dist_hash = hash * FIBONACCI_MULTIPLIER;
+    const auto top_bits = 64 - shard_bits;
+    const auto shard_index = static_cast<unsigned int>(dist_hash >> top_bits);
+    return shard_index;
+  }
 };
